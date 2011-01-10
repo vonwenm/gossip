6ec262035c972ae75d78c91c3ede296bfde07071
A Horn <ahorn@mcsoxford.org>
2011-01-10 18:56:21 +0000

    Expand functional tests and release resources after each test case.

--- udp_test.go
+++ udp_test.go
@@ -3,46 +3,101 @@ package transport
 import (
 	"testing"
 	"os"
+	"fmt"
+	"net"
 )
 
-const expectedByte byte = 0x42
+const expectedRequest = "Hi, I am client!"
+const expectedReply = "Nice to meet you. I am server!"
 
 var reply chan Message
 
-func TestConn(t *testing.T) {
+func TestClientServer(t *testing.T) {
 	reply = make(chan Message, 1)
+	defer close(reply)
 
-	startServer(t)
-	startClient(t)
+	const port uint = 9999
+
+	server := startServer(t, port)
+	defer server.Disconnect()
+
+	client := startClient(t, port)
+	defer client.Disconnect()
 
 	msg := <-reply
-	if len(msg) != 1 || msg[0] != expectedByte {
-		t.Fatalf("Unexpected response message from server.")
+	actualReply := string([]byte(msg))
+	if actualReply != expectedReply {
+		t.Fatalf("TestClientServer expected reply %q got %q.", expectedReply, actualReply)
 	}
 }
 
-func startServer(t *testing.T) {
+func TestPeer(t *testing.T) {
+	reply = make(chan Message, 1)
+	defer close(reply)
+
+	peer0 := startPeer(t, 9999)
+	defer peer0.Disconnect()
+	peer0.AddHandler(receiveReply)
+
+	peer1 := startPeer(t, 9988)
+	defer peer1.Disconnect()
+	peer1.AddHandler(sendReply)
+
+	msg := []byte(expectedRequest)
+	peer1Addr, err := net.ResolveUDPAddr(peer1.sock.LocalAddr().String())
+	if err != nil {
+		t.Fatalf("TestPeer could not resolve peer address: %s.", err)
+	}
+	peer0.SendTo(msg, peer1Addr)
+
+	msg = <-reply
+	actualReply := string([]byte(msg))
+	if actualReply != expectedReply {
+		t.Fatalf("TestPeer expected reply %q got %q.", expectedReply, actualReply)
+	}
+}
+
+// Starts and returns a connector which listens to the specified port on localhost
+func startPeer(t *testing.T, port uint) *Conn {
+	conn := NewConn()
+	go monitor(conn.Err, t)
+	err := conn.Listen(port)
+	if err != nil {
+		t.Fatalf("Cannot start peer: %q", err)
+		return nil
+	}
+	return conn
+}
+
+// Start a server which listens to the specified port on localhost
+func startServer(t *testing.T, port uint) *Conn {
 	conn := NewConn()
 	go monitor(conn.Err, t)
 	conn.AddHandler(sendReply)
-	err := conn.Listen(9999)
+	err := conn.Listen(port)
 	if err != nil {
 		t.Fatalf("Cannot start server: %q", err)
-		return
+		return nil
 	}
+	return conn
 }
 
-func startClient(t *testing.T) {
+// Start a client which sends packets to the specified port on localhost
+func startClient(t *testing.T, port uint) *Conn {
 	conn := NewConn()
 	go monitor(conn.Err, t)
 	conn.AddHandler(receiveReply)
-	if err := conn.Dial("127.0.0.1:9999"); err != nil {
+
+	addr := fmt.Sprintf("127.0.0.1:%d",port)
+	if err := conn.Dial(addr); err != nil {
 		t.Fatalf("Cannot connect to server: %q", err)
-		return
+		return nil
 	}
 
-	msg := []byte("Hi, there!")
+	msg := []byte(expectedRequest)
 	conn.Send(msg)
+
+	return conn
 }
 
 // Fail all tests if connector encounters error.
@@ -55,7 +110,12 @@ func monitor(errors <-chan os.Error, t *testing.T) {
 
 // Event handler which responds to the sender of an incoming packet with the expected message.
 func sendReply(conn *Conn, p *Packet) {
-	conn.SendTo([]byte{expectedByte}, p.Addr)
+	actualRequest := string([]byte(p.Msg))
+	if actualRequest == expectedRequest {
+		conn.SendTo([]byte(expectedReply), p.Addr)
+	} else {
+		conn.SendTo([]byte("Go away!"), p.Addr)
+	}
 }
 
 // Event handler to print the source address of an incoming packet.
